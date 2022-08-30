da93367302f809c24cf97106b82c682326895cd9
tobi <tobiaina96@gmail.com>
2022-08-30 12:08:55 +0100

    added circle ci

---
+++ run_kubernetes.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="tobiaina/microservice:v1.0.0"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run microservice \
+    --image=tobiaina/microservice:v1.0.0 \
+    --port=80 --labels app=microservice
+
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward microservice 8000:80
+
---
+++ upload_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="tobiaina/microservice:v1.0.0"
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag microservice $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
